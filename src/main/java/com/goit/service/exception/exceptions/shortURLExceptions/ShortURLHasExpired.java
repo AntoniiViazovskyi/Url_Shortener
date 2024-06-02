f237c7d8186bcd359db8aa737f862c4b13a3f6a8
Belimbb <belovmladshui@gmail.com>
2024-06-02 11:37:41 +0200

    LongURL custom exceptions added Added general exception Small refactoring some User and ShortURL exceptions

--- src/main/java/com/goit/service/exception/exceptions/shortURLExceptions/ShortURLHasExpired.java
+++ src/main/java/com/goit/service/exception/exceptions/shortURLExceptions/ShortURLHasExpired.java
@@ -3,9 +3,10 @@ package com.goit.service.exception.exceptions.shortURLExceptions;
 import java.time.LocalDate;
 
 public class ShortURLHasExpired extends Exception{
-    private static final String SHORT_URL_EXPIRED_BY_CREATION_TIME = "Short URL has expired." +
-            "Time of validity of short links is 30 days." +
-            "Creation time: %s";
+    private static final String SHORT_URL_EXPIRED_BY_CREATION_TIME = """
+            Short URL has expired. Time of validity of short links is 30 days.
+            Creation time: %s
+            """;
 
     public ShortURLHasExpired(LocalDate creationTime) {
     }
