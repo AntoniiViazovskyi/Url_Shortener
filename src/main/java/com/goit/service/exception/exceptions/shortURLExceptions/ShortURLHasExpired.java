2e166c9958092a068ddf82412b9ad03cf88ebd4a
Belimbb <belovmladshui@gmail.com>
2024-06-03 12:35:16 +0200

    Added to GlobalExceptionHandler all custom exceptions

--- src/main/java/com/goit/service/exception/exceptions/shortURLExceptions/ShortURLHasExpired.java
+++ src/main/java/com/goit/service/exception/exceptions/shortURLExceptions/ShortURLHasExpired.java
@@ -5,9 +5,9 @@ import java.time.LocalDate;
 public class ShortURLHasExpired extends Exception{
     private static final String SHORT_URL_EXPIRED_BY_CREATION_TIME = """
             Short URL has expired. Time of validity of short links is 30 days.
-            Creation time: %s
+            Your link was valid until %s
             """;
 
-    public ShortURLHasExpired(LocalDate creationTime) {
+    public ShortURLHasExpired(LocalDate expiryDate) {
     }
 }
